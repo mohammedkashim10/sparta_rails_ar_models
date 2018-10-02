bf06e1e4551f3ecf6f0265f6ed3f522879d7927f
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-02 17:04:25 +0100

    resources created and working separately

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -69,6 +69,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.fetch(:car, {})
+      params.require(:car).permit(:make, :modelname, :year)
     end
 end
