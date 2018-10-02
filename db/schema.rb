fef6d6eb8e4ff450a57477d61bdd36f601ea706b
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-02 17:25:00 +0100

    connected resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_150539) do
+ActiveRecord::Schema.define(version: 2018_10_02_160831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_150539) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "country_id"
   end
 
   create_table "countries", force: :cascade do |t|
